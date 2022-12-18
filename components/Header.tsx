fee8058afd2ec3e7b698a105d7cdc767b9f89d46
Mizanur Rahman <nickty.86@gmail.com>
2022-12-18 19:18:52 +0600

    Header created

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,28 @@
+import Image from 'next/image'
 import React from 'react'
+import styles from '../styles/Home.module.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={styles.header}>
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                <Image src='/logo.png' width="128" height="65" />
+            </div>
+            <div className={styles.menu}>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className={styles.topEnd}>
+                Seach and cart
+            </div>
+        </div>
+    </div>
   )
 }
 
